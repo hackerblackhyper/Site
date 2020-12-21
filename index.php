3597305ca09cf1050b503dca7c3712b6c46c310c
hsyn-ghb <46356650+hackerblackhyper@users.noreply.github.com>
2020-12-21 18:16:01 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang=tr-TR>
+<head>
+<meta http-equiv="Content-Type"content="text/html"; charset="utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>DOĞA MANZARALARI</title>
+<link href="stil.css" rel="stylesheet">
+<link href="fonts/stylesheet.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    
+<header class="ust">
+    <h1><strong><a href="index.php">DOĞA</a></strong> MANZARALARI</h1>
+    <nav id="nav">
+    <ul>
+    <li><a href="#">ANASAYFA</a></li>    
+    <li><a href="#">ELEMENTLER</a></li>    
+    <li><a href="#">DOĞA</a></li>    
+    </ul>
+    </nav>
+    
+    <!-------------HÜSEYİN DURMUŞ-------->
+    <section id="banner">
+    <h2>DOĞA</h2>
+    <p>Doğa Manzaraları için doğru adrestesiniz.</p>
+    <a href="#">BAŞLIYALIM</a>
+    </section>
+</header>
+    
+    <section id="bolum2">
+    <div class="kapsayici">
+        <div class="sol">
+        <h2>DOĞA</h2>
+            <p>Doğa; tabiat, çevre, maddesel dünya.</p>
+        </div>
+        <div class="sag">
+        <article><p><b>Doğa;</b> kendini sürekli olarak yenileyen ve değiştiren, canlı ve cansız maddelerden oluşan varlıkların hepsini kapsar. İnsani faktörler etkin değildir. Madde ve enerji unsurlarından oluştuğu kabul edilir.<br><br>
+        <b>Doğa,</b> en geniş tanımıyla doğal, fiziksel, maddi dünya, ve aslında tüm evrendir. Doğa, fiziksel dünyanın olgusuna ve genel olarak yaşamada işaret edebilir. Doğayı incelemek doğa bilimlerinin kategorisidir. 
+          </p></article>
+        </div>
+      </div>
+    </section>
+    <div class="temizle"></div>
+    <!-------------HÜSEYİN DURMUŞ-------->
+    <section id="bolum3">
+        <h2>MANZARANIN EN GÜZEL EŞSİZLİĞİ</h2>
+        <p>MANZARA VE DOĞA HARİKALARI</p>
+        <div class="kapsayici1"></div>
+        <div class="resim"><img src="Resimler/yol4.jpg" alt="Resimlerimiz">
+        <p>YOL BOYU UZANAN KUM VE SAHİLİ</p>
+        </div>
+        <div class="resim"><img src="Resimler/doga.jpg" alt="Doğa Resimlerimiz">
+        <p>DOĞA'NIN EŞŞİZ MANZARASI</p>
+        </div>
+        <div class="temizle"></div>
+    <ul class="yonlendir">
+        <li><a href="#">TANIYALIM</a></li>
+        <li><a href="#">GÖRELİM</a></li>
+    </ul>
+    </section>
+    <section id="bolum4">
+    <h2>Doğa Yorumlayıcıları</h2>
+    <p>Doğa manzaralarımızı yorumlayanlar</p>
+    <div class="kapsayici">
+        <div class="bolumler">
+        <div class="kucukresim"><img src="Resimler/ev1.jpg" alt="doğa"></div>
+        <div class="yazi">
+            <h3>Doğa Yorumları</h3>
+            <p>Doğa Manzaralarınız çok güzel ellerinize sağlık.Harika bir web siteniz var herkese tavsiye edicem daha iyi çalışmalar dileğiyle</p>
+            </div>
+        </div>
+        <div class="bolumler">
+        <div class="kucukresim"><img src="Resimler/yol3.jpg" alt="doğa"></div>
+        <div class="yazi">
+            <h3>Doğa Yorumları</h3>
+            <p>Doğa Manzaralarınız çok güzel ellerinize sağlık.Harika bir web siteniz var herkese tavsiye edicem daha iyi çalışmalar dileğiyle</p>
+            </div>
+        </div>
+        <div class="bolumler">
+        <div class="kucukresim"><img src="Resimler/yol.jpg" alt="doğa"></div>
+        <div class="yazi">
+            <h3>Doğa Yorumları</h3>
+            <p>Doğa Manzaralarınız çok güzel ellerinize sağlık.Harika bir web siteniz var herkese tavsiye edicem daha iyi çalışmalar dileğiyle</p>
+            </div>
+        </div>
+        <div class="bolumler">
+        <div class="kucukresim"><img src="Resimler/doga2.jpg" alt="doğa"></div>
+        <div class="yazi">
+            <h3>Doğa Yorumları</h3>
+            <p>Doğa Manzaralarınız çok güzel ellerinize sağlık. Harika bir web siteniz var herkese tavsiye edicem daha iyi çalışmalar dileğiyle</p>
+            </div>
+        </div>
+    </div>
+    </section>
+    <div class="temizle"></div>
+    <section id="bolum5">
+    <div class="icerik">
+        <h2>DOĞA MANZARALARI</h2>
+        <p>BİZİ TAKİP EDİN</p><br>
+
+        <a href="#">TAKİP EDİN</a>
+        </div>
+    </section>
+    <footer>
+    <div class="sosyal">
+        <a href="#" title="facebook"></a>
+        <a href="#" title="twitter"></a>
+        <a href="https://www.linkedin.com/in/h%C3%BCseyin-durmu%C5%9F-268842165/" title="linkedin"></a>
+        <ul>
+        <li><p>DOĞA</p></li>
+
+        <li><p>HÜSEYİN DURMUŞ</p></li>
+
+        <li><p>MANZARALAR</p></li>
+
+        </ul>
+        </div>
+    </footer>
+</body>
+</html>
+
